bef1daa21eeff699124511e6f2544ddb8fd40d52
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-28 17:52:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,7 @@ class Application
         end
       else
         resp.write "Your cart is empty"
+      end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       if @@items.include?(add_item)
