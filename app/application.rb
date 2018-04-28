6b82b0a254ab26ecf8f0c7222dac91c83a1b57ed
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-28 17:50:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,7 @@ class Application
       add_item = req.params["item"]
       if @@items.include?(add_item)
         @@cart << add_item
+        resp.write "added #{add_item}"
       else
         resp.write "Your cart is empty"
       end
